63160f42c71684fe98ce1f2745a21ea9e760ef07
Marcus Lull <marcuslull@gmail.com>
2023-06-18 08:57:53 -0700

    adding some comments to binary search

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,4 +1,5 @@
 class BinarySearch:
+    """Binary Search Class - Worst case time complexity: O(logN)"""
 
     def __init__(self, container_to_search, key_to_search_for):
         self.container_to_search = container_to_search
@@ -13,11 +14,10 @@ class BinarySearch:
 
         while high_pointer >= low_pointer:
             mid_pointer = (high_pointer + low_pointer) // 2
-            # print("searching index:", self.mid_pointer)
-            if self.container_to_search[mid_pointer] < self.key_to_search_for:
+            if self.container_to_search[mid_pointer] < self.key_to_search_for:  # search the upper half
                 low_pointer = mid_pointer + 1
-            elif self.container_to_search[mid_pointer] > self.key_to_search_for:
+            elif self.container_to_search[mid_pointer] > self.key_to_search_for:  # search the lower half
                 high_pointer = mid_pointer - 1
             else:
                 return mid_pointer
-        return -1
+        return -1  # key not found
