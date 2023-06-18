bd4250334c4426cc1596f1b94c2421d4993aec45
Marcus Lull <marcuslull@gmail.com>
2023-06-18 09:10:24 -0700

    removed a comment from linear

--- search/linear_search.py
+++ search/linear_search.py
@@ -9,7 +9,6 @@ class LinearSearch:
         """returns the position of the key if found or -1 if none does not work on dicts or sets"""
 
         for i in range(len(self.container_to_search)):
-            # print("searching index:", i)
             if self.container_to_search[i] == self.key_to_search_for:
                 return i
         return -1
